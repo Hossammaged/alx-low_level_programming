92676d11fbdffdaf551cd5bd8766d5dbc05128e2
Hossammaged <hossammaged54@gmail.com>
2023-03-16 11:23:47 -0700

    5,6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return:Always 0 (success)
  */
 
-int main(viod)
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
@@ -13,3 +13,4 @@ int main(viod)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
+}
