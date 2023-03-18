6b3991159c33e24139b01be96ec41e1ac6b73168
Hossammaged <hossammaged54@gmail.com>
2023-03-18 10:42:57 -0700

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of three digits  no 0 , no 1 , no 2
+ * Return: Alwas 0 (success)
+ */
+
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0' ; hundred <= '9' ; hundred++) /*hundred place*/
+	{
+		for (ten = (hundred + 1) ; ten <= '9' ; ten++) /*ten=100s+1*/
+		{
+			for (one = (ten + 1) ; one <= '9' ; one++) /*ones*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
