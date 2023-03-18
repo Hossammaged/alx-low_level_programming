02d8fa00dbe8976168ad5754f0985137677f7456
Hossammaged <hossammaged54@gmail.com>
2023-03-18 07:46:36 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in new line.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		printf(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
